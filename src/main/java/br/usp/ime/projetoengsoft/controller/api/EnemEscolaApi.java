4e708e3e2a17f8d49a4c1f670d41e7946662f66e
Joao Pedro <joao.pedro.petrosino.costa@gmail.com>
2024-12-01 18:56:24 -0300

    fiz: anotations para CORS

--- src/main/java/br/usp/ime/projetoengsoft/controller/api/EnemEscolaApi.java
+++ src/main/java/br/usp/ime/projetoengsoft/controller/api/EnemEscolaApi.java
@@ -12,12 +12,9 @@ import br.usp.ime.projetoengsoft.service.EnemEscolaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/enem-escola")
 public class EnemEscolaApi {
